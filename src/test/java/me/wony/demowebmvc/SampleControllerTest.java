be006679c6ca0bd8c221bb3c676443f3e31d0cc5
yoonemong <yoonemong@naver.com>
2020-03-19 07:59:37 +0900

    mvc

---
+++ src/test/java/me/wony/demowebmvc/SampleControllerTest.java
@@ -0,0 +1,31 @@
+package me.wony.demowebmvc;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void helloTest() throws  Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/hi.json"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello sungwon"))
+                .andExpect(MockMvcResultMatchers.handler().handlerType(SampleController.class))
+                .andExpect(MockMvcResultMatchers.handler().methodName("hello"));
+
+    }
+}
