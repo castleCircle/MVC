affa1ab6ba8b1d9b9bab6298955a1480d735532b
yoonemong <yoonemong@naver.com>
2020-03-20 07:25:13 +0900

    mvc study

--- src/main/java/me/wony/demowebmvc/SampleController.java
+++ src/main/java/me/wony/demowebmvc/SampleController.java
@@ -1,20 +1,42 @@
 package me.wony.demowebmvc;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(method = RequestMethod.GET)
 public class SampleController {
 
-//    @RequestMapping(value = "/hello" , method = {RequestMethod.GET,RequestMethod.PUT})
-    @RequestMapping({"/hello/**","/hi","/hi.*"})
+    @GetMapping(value = "/hello" ,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE ,produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
     public String hello(){
         return "hello sungwon";
     }
 
+        @RequestMapping(value = "/hello1" , headers = HttpHeaders.FROM , params = "name")
+    @ResponseBody
+    public String hello1(){
+        return "hello sungwon";
+    }
+
+
+    @GetMapping(value = "/hello2")
+    @ResponseBody
+    public String hello2(){
+        return "hello sungwon";
+    }
+
+    @PostMapping(value = "/hello2")
+    @ResponseBody
+    public String hello3(){
+        return "hello sungwon";
+    }
+
+    @GetHelloMapping
+    @ResponseBody
+    public String hello4(){
+        return "hello";
+    }
 }
--- src/test/java/me/wony/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/wony/demowebmvc/SampleControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,12 +22,29 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws  Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get("/hi.json"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello")
+        .contentType(MediaType.APPLICATION_JSON_UTF8)
+        .accept(MediaType.TEXT_PLAIN_VALUE))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello1")
+                .header(HttpHeaders.FROM,1).param("name","t"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders.options("/hello2"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("hello sungwon"))
-                .andExpect(MockMvcResultMatchers.handler().handlerType(SampleController.class))
-                .andExpect(MockMvcResultMatchers.handler().methodName("hello"));
+                .andExpect(MockMvcResultMatchers.header().stringValues(HttpHeaders.ALLOW,"GET,HEAD,POST,OPTIONS"));
 
-    }
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello5"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        }
+
+
+
 }
